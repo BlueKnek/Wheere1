48a435064c515d778ca8ae242815f63c62b7d391
BlueKnek <blueknek@gmail.com>
2018-01-13 13:54:14 +0100

    db uses file on disk

--- db.py
+++ db.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import sessionmaker
 
 from schema import Base
 
-engine = create_engine('sqlite:///:memory:', echo=True)
-Base.metadata.create_all(engine)
+engine = create_engine('sqlite:///db.sqlite3', echo=True)
 Session = sessionmaker(bind=engine)
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
